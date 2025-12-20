683f24dd304276f927eec1e4cbbec64e952d9bb2
DevaunThomas <devaunthomas@gmail.com>
2025-12-20 16:25:54 -0500

    Introducing files for program

---
+++ include/models/Player.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "StatLine.h"
+
+class Player
+{
+public:
+    Player(int id, )
+};
---
+++ include/models/StatLine.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstdint>
+
+struct StatLine
+{
+    int points = 0;
+    int rebounds = 0;
+    int assists = 0;
+    int steals = 0;
+    int blocks = 0;
+    int FGAttempts = 0;
+    int FGMade = 0;
+    int threesAttempted = 0;
+    int threesMade = 0;
+    int FTAttempts = 0;
+    int FTMade = 0;
+    int turnovers = 0;
+    int minutesPlayed = 0;
+}
